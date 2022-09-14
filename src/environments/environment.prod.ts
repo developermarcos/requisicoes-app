3c29d4af719cd64cfca56a603bf2d190d371d537
Marcos Adriano Ribeiro Lima <developermarcos@outlook.com>
2022-09-14 13:18:50 -0300

    environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyCUBEAc6OTwRCB1TBzO_OhWakWOBKFGK24",
+    authDomain: "requisicoes-app-87f19.firebaseapp.com",
+    projectId: "requisicoes-app-87f19",
+    storageBucket: "requisicoes-app-87f19.appspot.com",
+    messagingSenderId: "813409485387",
+    appId: "1:813409485387:web:40bc64cdac01f9d063c3ce"
+  }
 };
